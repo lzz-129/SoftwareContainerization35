d9c8232ee8d3f2eb37ea2bbb63ec9c3978b4ddb3
lzz-129 <3044178018@qq.com>
2024-01-30 00:50:48 +0100

    docker web api

---
+++ film_backend/conf.py
@@ -0,0 +1,12 @@
+## database configs
+DBADDR = '0.0.0.0'  # Docker 容器映射到宿主机的地址
+DATABASE = 'moviesmanagement'   # 默认数据库名称
+DBUSER = 'postgres'   # 默认用户名
+DBPW = 'mysecretpassword'      # 设置的密码
+DBPORT = 5432          # 映射到宿主机的端口
+
+#HOST IP
+HOST = '0.0.0.0'
+
+#PORT
+PORT = 12345
